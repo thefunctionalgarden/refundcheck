6bd9bf89663ad4d801b4d9dd6896d482293a031c
Camilo Cerchiari <camilo.cerchiari@superbet.com>
2023-07-03 15:14:28 -0300

    oauth support

---
+++ src/refundcheck_auth_handler.erl
@@ -0,0 +1,301 @@
+-module(refundcheck_auth_handler).
+
+-behavior(cowboy_handler).
+
+% -include_lib("kernel/include/logger.hrl").
+-include_lib("kernel/include/file.hrl").
+
+-export([
+    init/2,
+    allowed_methods/2,
+    % content_types_accepted/2
+    content_types_provided/2
+]).
+-export([
+    % from_json/2
+    to_send/2
+]).
+
+
+init(Req0, State) ->
+    {cowboy_rest, Req0, State}.
+
+
+% valid_content_headers(Req, State) ->
+%     {true, Req, State}.
+
+allowed_methods(Req0, State) ->
+    Req = cowboy_req:set_resp_headers(#{
+        <<"Access-Control-Allow-Origin">> => <<"*">>,
+        <<"Access-Control-Allow-Methods">> => <<"GET, HEAD">>,
+        <<"Access-Control-Allow-Headers">> => <<"Content-Type">>
+    }, Req0),
+    {[<<"GET">>, <<"HEAD">>, <<"OPTIONS">>], Req, State}.
+
+% content_types_accepted(Req, State) ->
+%     %%  define AcceptCallback callback for PUT, POST, PATCH
+%     Handler = [
+%         {<<"application/json">>, from_json}
+%     ],
+%     {Handler, Req, State}.
+
+content_types_provided(Req, State) ->
+    %%  define ProvideResource callback (ProvideCallback) for GET, HEAD
+    Handler = [
+        {<<"text/html">>, to_send}
+    ],
+    {Handler, Req, State}.
+
+
+
+% %% AcceptCallback   (for PUT, POST, PATCH)
+% %%      Result  :: true
+% %%               | {created, URI :: iodata()}
+% %%               | {see_other, URI :: iodata()}
+% %%               | false
+% from_json(Req0, State) ->
+%     {ok, OrgBodyEnc, Req1} = cowboy_req:read_body(Req0),
+%     OrgBody = jsx:decode(OrgBodyEnc, [return_maps]),
+%     RespBody = processData(OrgBody),
+%     RespBodyEnc = jsx:encode(RespBody),
+%     ReqN = cowboy_req:set_resp_body(RespBodyEnc, Req1),
+%     {stop, ReqN, State}.  %%  {Result, Req, State}
+
+
+%% ProvideCallback   (for GET, HEAD)
+%%      Result :: cowboy_req:resp_body()
+to_send(Req0, State) ->
+    io:format("~p:~p ~n", [?MODULE, ?LINE]),
+    Path = cowboy_req:path(Req0),
+    case Path of
+        <<"/login">> ->
+            StateToken = base64:encode(crypto:strong_rand_bytes(32)),
+            RedirectURL = oauth_step1(StateToken),
+            io:format("~p:~p ~n", [?MODULE, ?LINE]),
+
+            ReqN = cowboy_req:reply(
+                303,
+                #{ <<"location">> => RedirectURL },
+                Req0
+            );
+
+        <<"/login_callback">> ->
+            #{code := Code} = cowboy_req:match_qs([{code, [], undefined}], Req0),
+            OAuthData = oauth_step2(Code),
+            io:format("~p:~p OAuthData:~p ~n", [?MODULE, ?LINE, OAuthData]),
+            
+            PeopleData = getPeopleData(maps:get(access_token, OAuthData)),
+            io:format("~p:~p PeopleData:~p ~n", [?MODULE, ?LINE, PeopleData]),
+
+            ReqN = Req0
+
+    end,
+
+    {stop, ReqN, State}.  %%  {Result, Req, State}
+
+    
+
+
+%% -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+
+% service_account() ->
+%     io:format("~p:~p ~n", [?MODULE, ?LINE]),
+%     application:ensure_all_started(oauth2c),
+%     application:ensure_all_started(ssl),
+%     io:format("~p:~p ~n", [?MODULE, ?LINE]),
+
+%     F = <<"/home/cam/sec/oauth2/service-account-key-sellers-guard-dd25e33776e0.json">>,
+%     Scope = <<"https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile">>,
+
+%     Client0 = oauth2c:from_service_account_file(F, Scope),
+%     {ok, _Headers, Client1} = oauth2c:retrieve_access_token(Client0, []),
+
+%     io:format("~p:~p Client:~p ~n", [?MODULE, ?LINE, Client1]),
+
+%     ok.
+
+
+oauth_step1(StateToken) ->
+    io:format("~p:~p ~n", [?MODULE, ?LINE]),
+    application:ensure_all_started(restc),
+    io:format("~p:~p ~n", [?MODULE, ?LINE]),
+
+    % https://developers.google.com/identity/protocols/oauth2/web-server
+    OAuth2Endpoint = refundcheck_config:getAuthEndpoint(),
+    OAuth2Path = refundcheck_config:getAuthPath(),
+    Q1 = {"client_id", refundcheck_config:getAuthClientId()},
+    Q2 = {"redirect_uri", refundcheck_config:getAuthRedirectURILoginCallback()},
+    Q3 = {"response_type", <<"code">>},
+    Q4 = {"scope", <<"https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile">>},
+    Q5 = {"access_type", <<"online">>},
+    Q6 = {"state", StateToken}, % save this state and verify it comes untouched after the redirection in step2
+    Q7 = {"include_granted_scopes", <<"true">>},
+    % Q8 = {"login_hint",},
+    % Q9 = {"prompt",},
+ 
+    RedirectURL = restc:construct_url(OAuth2Endpoint, OAuth2Path, [Q1, Q2, Q3, Q4, Q5, Q6, Q7]),
+ 
+    io:format("~p:~p RedirectURL:~p ~n", [?MODULE, ?LINE, RedirectURL]),
+
+    RedirectURL.
+
+
+oauth_step2(undefined) ->
+    #{
+        access_token  => undefined,
+        expires_in    => undefined,
+        refresh_token => undefined
+    };
+oauth_step2(Code) ->
+    %TODO verificar state <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< TODO%
+    OAuth2TokenEndpoint = refundcheck_config:getAuthTokenEndpoint(),
+    Q1 = {"client_id", refundcheck_config:getAuthClientId()},
+    Q2 = {"client_secret", refundcheck_config:getAuthClientSecret()},
+    Q3 = {"code", Code},
+    Q4 = {"grant_type", <<"authorization_code">>},
+    Q5 = {"redirect_uri", refundcheck_config:getAuthRedirectURIHome()},
+    ContentType = <<"application/x-www-form-urlencoded">>,
+
+    TokenURL = restc:construct_url(OAuth2TokenEndpoint, "", [Q1, Q2, Q3, Q4, Q5]),
+    OAuthData = case restc:request(post, ContentType, TokenURL, []) of
+        {ok, 200, _H, RespBody} ->
+            % {
+            %     "access_token": "1/fFAGRNJru1FTz70BzhT3Zg",
+            %     "expires_in": 3920,
+            %     "token_type": "Bearer",
+            %     "scope": "https://www.googleapis.com/auth/drive.metadata.readonly",
+            %     "refresh_token": "1//xEoDL4iW3cxlI7yDbSRFYNG01kVKM2C-259HOF2aQbI"
+            % }
+            AccessToken  = maps:get("access_token",  RespBody, undefined),
+            ExpiresIn    = maps:get("expires_in",    RespBody, undefined),
+            RefreshToken = maps:get("refresh_token", RespBody, undefined),
+
+            #{
+                access_token  => AccessToken,
+                expires_in    => ExpiresIn,
+                refresh_token => RefreshToken
+            };
+
+        _Other ->
+            #{
+                access_token  => undefined,
+                expires_in    => undefined,
+                refresh_token => undefined
+            }
+    end,
+    OAuthData.
+
+getPeopleData(AccessToken) ->
+    PeopleEndpoint     = <<"https://people.googleapis.com/v1/">>,
+    PeopleResourceName = <<"people/me">>,
+    Q1 = {"personFields", <<"names,emailAddresses">>},
+    PeopleURL = restc:construct_url(PeopleEndpoint, PeopleResourceName, [Q1]),
+    Headers = [{<<"Authorization">>, <<"Bearer ", AccessToken/bitstring>>}],
+
+    PeopleData = case restc:request(get, json, PeopleURL, [200], Headers) of
+        {ok, 200, _H, RespBody} ->
+            #{
+                name  => getPeopleName(RespBody),
+                email => getPeopleEmail(RespBody)
+            }
+    end,
+    PeopleData.
+
+getPeopleName(#{"names" := Names}) ->
+    getPrimaryName(Names).
+
+getPrimaryName([]) ->
+    <<"">>;
+getPrimaryName([#{"displayName" := DisplayName, "metadata" := #{"primary" := true}} | _MoreNames]) ->
+    DisplayName;
+getPrimaryName([_NotPrimary | MoreNames]) ->
+    getPrimaryName(MoreNames).
+
+
+getPeopleEmail(#{"emailAddresses" := Emails}) ->
+    getPrimaryEmail(Emails).
+
+getPrimaryEmail([]) ->
+    <<"">>;
+getPrimaryEmail([#{"value" := PrimaryEmail, "metadata" := #{"primary" := true}} | _MoreEmails]) ->
+    PrimaryEmail;
+getPrimaryEmail([_NotPrimary | MoreEmails]) ->
+    getPrimaryEmail(MoreEmails).
+
+%% -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+% {
+%   "resourceName": "people/114828984764373600194",
+%   "etag": "%EgcBAgkuNz0+GgQBAgUHIgxOSFA2MkZPTmt2az0=",
+%   "names": [
+%     {
+%       "metadata": {
+%         "primary": true,
+%         "source": {
+%           "type": "PROFILE",
+%           "id": "114828984764373600194"
+%         },
+%         "sourcePrimary": true
+%       },
+%       "displayName": "Camilo",
+%       "givenName": "Camilo",
+%       "displayNameLastFirst": "Camilo",
+%       "unstructuredName": "Camilo"
+%     },
+%     {
+%       "metadata": {
+%         "source": {
+%           "type": "CONTACT",
+%           "id": "f"
+%         }
+%       },
+%       "displayName": "Yo",
+%       "givenName": "Yo",
+%       "displayNameLastFirst": "Yo",
+%       "unstructuredName": "Yo"
+%     }
+%   ],
+%   "emailAddresses": [
+%     {
+%       "metadata": {
+%         "primary": true,
+%         "verified": true,
+%         "source": {
+%           "type": "ACCOUNT",
+%           "id": "114828984764373600194"
+%         },
+%         "sourcePrimary": true
+%       },
+%       "value": "camilo.cerchiari@gmail.com"
+%     },
+%     {
+%       "metadata": {
+%         "verified": true,
+%         "source": {
+%           "type": "ACCOUNT",
+%           "id": "114828984764373600194"
+%         }
+%       },
+%       "value": "cam@montevideo.com.uy"
+%     },
+%     {
+%       "metadata": {
+%         "source": {
+%           "type": "CONTACT",
+%           "id": "f"
+%         },
+%         "sourcePrimary": true
+%       },
+%       "value": "camilo.cerchiari@gmail.com",
+%       "type": "other",
+%       "formattedType": "Other"
+%     }
+%   ]
+% }
+
+%% -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+
+   
+   
--- src/refundcheck_config.erl
+++ src/refundcheck_config.erl
@@ -5,7 +5,14 @@
     getDBUser/0,
     getDBPass/0,
     getRoutes/0,
-    getHTTPPort/0
+    getHTTPPort/0,
+    getAuthEndpoint/0,
+    getAuthPath/0,
+    getAuthTokenEndpoint/0,
+    getAuthClientId/0,
+    getAuthClientSecret/0,
+    getAuthRedirectURILoginCallback/0,
+    getAuthRedirectURIHome/0
 ]).
 
 getDBHost() ->
@@ -28,3 +35,39 @@ getHTTPPort() ->
     S = application:get_env(refundcheck, http_port, "2020"),
     V = list_to_integer(S),
     V.
+
+% -- -- -- -- -- -- -- -- -- -- --
+
+getAuthEndpoint() ->
+    V = application:get_env(refundcheck, auth_endpoint, undefined),
+    V.
+
+getAuthPath() ->
+    V = application:get_env(refundcheck, auth_path, undefined),
+    V.
+
+getAuthTokenEndpoint() ->
+    V = application:get_env(refundcheck, auth_token_endpoint, undefined),
+    V.
+
+getAuthClientId() ->
+    V = application:get_env(refundcheck, auth_client_id, undefined),
+    V.
+
+getAuthClientSecret() ->
+    V = application:get_env(refundcheck, auth_client_secret, undefined),
+    V.
+
+getAuthRedirectURILoginCallback() ->
+    V = application:get_env(refundcheck, auth_redirect_uri_login_callback, undefined),
+    V.
+
+getAuthRedirectURIHome() ->
+    V = application:get_env(refundcheck, auth_redirect_uri_home, undefined),
+    V.
+
+
+
+    
+% -- -- -- -- -- -- -- -- -- -- --
+
